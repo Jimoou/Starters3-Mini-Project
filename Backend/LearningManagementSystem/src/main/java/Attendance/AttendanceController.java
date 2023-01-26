4e0668abb591359f1318576797e55b6996274372
kimsohyeon67 <kimsohyeon67@gmail.com>
2023-01-26 18:01:48 +0900

    feature/#11 출결 정보 가져오기

--- Backend/LearningManagementSystem/src/main/java/Attendance/AttendanceController.java
+++ Backend/LearningManagementSystem/src/main/java/Attendance/AttendanceController.java
@@ -1,59 +1,75 @@
 package Attendance;
 
-import jakarta.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
-enum AttendanceStatus {ABSENCE, ATTENDANCE, LATENESS, SICK_LEAVE};
 
 @RestController
 public class AttendanceController {
-
     @Autowired
     @Qualifier("attendanceservice")
     AttendanceService service;
-
+    
+    Map<String, String> staus = Map.of(
+        "0", "결석",
+        "1", "출석",
+        "2", "지각",
+        "3", "병결"
+    );
     //전체 출석내역 확인
     @GetMapping("/attendance")
-    public List<AttendanceDTO> GetAttendnaceList() {
-        ModelAndView model = new ModelAndView();
-        List<AttendanceDTO> attendanceList = service.attendancelist();
-        return attendanceList;
+    public Object GetAttendnaceList() {
+
+        List<AttendanceUserDTO> attendanceList = service.attendancelist();
+
+        List<Object> attendanceAllList = new ArrayList<>();
+        HashSet<String> nameSet = new HashSet<String>();
+
+        for (AttendanceUserDTO attendance : attendanceList) {
+            nameSet.add((String) attendance.getUser_Name());
+        }
+        for(String onename :nameSet) {
+            Map<String, Object> attendanceAllMap = new HashMap<>();
+            attendanceAllMap.put("User_name", onename);
+
+            Map<String, String> attendance = new HashMap();
+            for (AttendanceUserDTO one : attendanceList) {
+                if(onename.equals(one.getUser_Name()))
+                {
+                    String date = one.getAttendance_Date() + " " + one.getAttendance_Time();
+                    String AttendanceStatus = staus.get(one.getAttendance_Status());
+                    attendance.put(date,AttendanceStatus);
+                }
+            }
+            attendanceAllMap.put("Attendance", attendance);
+            attendanceAllList.add(attendanceAllMap);
+        }
+        return attendanceAllList;
     }
 
-    // 유저 하나 출결 정보
-    @GetMapping("/attendace/{id}/1")
-    public List<AttendanceDTO> GetUserAttendnace(@PathVariable String id) {
-        List<AttendanceDTO> attendance = service.userAttendance(id);
-        return attendance;
-    }
-
-    // 미출석 유저 하나 출결 정보
+    //유저 하나 출결 정보
     @GetMapping("/attendace/{id}/2")
-    public List<AttendanceDTO> GetUserunAttendnace(@PathVariable String id) {
-        System.out.println(id);
-        List<AttendanceDTO> unattendance = service.userUnAttendance(id);
-        return unattendance;
-    }
-    //미출석 날짜 출력
-//    @GetMapping("/attendance/{UserId}/1")
-//    public List<Attendance>(@PathVariable String UserId, HttpServletRequest request) {
-//
-//        HttpSession session = request.getSession();
-//        AttendanceStatus attendance_Status;
-//
-////        if (session.getAttribute("attendance_Status") == null) {
-////          //에러페이지 출력
-////        }
-//
-//        //attendance_Status = (AttendanceStatus) session.getAttribute("attendance_Status");
-//
-//
-//    }
+    public Object GetUserunAttendnace(@PathVariable String id) {
+        List<AttendanceUserDTO> unattendance = service.userAttendance(id);
+        HashMap<Object,Object> oneUser = new HashMap<Object, Object>();
+        HashMap<Object,Object> attendance = new HashMap<Object, Object>();
 
+        for(AttendanceUserDTO one: unattendance)
+        {
+            String date = one.getAttendance_Date() + " " + one.getAttendance_Time();
+            String AttendanceStatus = staus.get(one.getAttendance_Status());
+            attendance.put(date,AttendanceStatus);
+        }
+
+        oneUser.put("user_Name",unattendance.get(0).getUser_Name());
+        oneUser.put("attendance",attendance);
+        return oneUser;
+    }
 }
--- Backend/LearningManagementSystem/src/main/java/Attendance/AttendanceServiceImpl.java
+++ Backend/LearningManagementSystem/src/main/java/Attendance/AttendanceServiceImpl.java
@@ -6,13 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-//dao-인터페이스(메서드이름) --- (스프링부트-SQLMAPPER 기능) --- sql.mapping.xml id 동일
-/*
- * 이전 방식이 Mapper 설정 파일(xml)에 있는 쿼리문의 ID를 직접 String 형태로 지정해서 사용했다면, 
- * Mapper 인터페이스를 사용한 방식은 인터페이스와 Mapper 설정 파일을 연동해두고 
- * 쿼리문의 ID와 동일한 메소드를 만들어둠으로써 
- * 해당 메소드를 호출하면 자동으로 쿼리문이 실행되도록 해주는 방식입니다
- * */
 @Service("attendanceservice")
 public class AttendanceServiceImpl implements AttendanceService {
 
@@ -25,17 +18,13 @@ public class AttendanceServiceImpl implements AttendanceService {
 	}
 
 	@Override
-	public List<AttendanceDTO> attendancelist() {
+	public List<AttendanceUserDTO> attendancelist() {
 		return dao.attendancelist();
 	}
 
 	@Override
-	public List<AttendanceDTO> userAttendance(String id) {
+	public List<AttendanceUserDTO> userAttendance(String id) {
 		return dao.userAttendance(id);
-	}
-	@Override
-	public List<AttendanceDTO> userUnAttendance(String id) {
-		return dao.userUnAttendance(id);
 	}
 
 }
