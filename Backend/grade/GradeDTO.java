0c6f423c66a117c4a63ef5922a31bc6e011963d7
new-h2 <79350383+new-h2@users.noreply.github.com>
2023-01-26 09:50:10 +0900

    /feature/#10 GradeDTO 성적관리

---
+++ Backend/grade/GradeDTO.java
@@ -0,0 +1,19 @@
+package  com.example.LearningManagementSystem.grade;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component("gradedto")
+public class GradeDTO {
+	
+	int grade_id;	//성적번호
+	String user_id;	//구성원id
+	int grade_type;	//시험타입
+	int grade_score; //점수
+	String grade_at; // 시험날짜
+	String grade_date; //평가날짜
+}
