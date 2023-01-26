6e5d7b4d7da57d76c692adaf9c5b3ec4975e6826
Jiwoon <kfromh0136@Naver.com>
2023-01-26 23:56:29 +0900

    #20-Mybatis설정파일&Etc.
    마이바티스 설정파일과 스케줄링 기능 삭제했습니다.

--- Backend/LearningManagementSystem/src/main/java/com/example/LearningManagementSystem/LearningManagementSystemApplication.java
+++ Backend/LearningManagementSystem/src/main/java/com/example/LearningManagementSystem/LearningManagementSystemApplication.java
@@ -3,12 +3,8 @@ package com.example.LearningManagementSystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@ComponentScan(basePackages ={"Attendance"})
 @MapperScan(basePackages = {"com/example/LearningManagementSystem/Attendance"})
 public class LearningManagementSystemApplication {
 
