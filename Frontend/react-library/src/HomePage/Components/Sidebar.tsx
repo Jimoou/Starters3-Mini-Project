663512e0017b5983c4b388e4bda92a29dd76bcf1
Jiwoon <kfromh0136@Naver.com>
2023-01-26 09:38:21 +0900

    #6-Layout
    레이아웃컴포넌트입니다.

--- Frontend/react-library/src/HomePage/Components/Sidebar.tsx
+++ Frontend/react-library/src/HomePage/Components/Sidebar.tsx
@@ -3,19 +3,25 @@ import GradingIcon from "@mui/icons-material/Grading";
 import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
 import RuleIcon from "@mui/icons-material/Rule";
 import PeopleIcon from "@mui/icons-material/People";
+import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 
 export const Sidebar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.menu}>
-        <div className={styles.container2}>
+        <Link to="/home" className={styles.container2}>
+          <HomeIcon className={styles.Icon} />
+          <div className={styles.menudata}>홈</div>
+        </Link>
+        <Link to="/home" className={styles.container2}>
           <GradingIcon className={styles.Icon} />
           <div className={styles.menudata}>성적 관리</div>
-        </div>
-        <div className={styles.container2}>
+        </Link>
+        <Link to='/answer' className={styles.container2}>
           <QuestionAnswerIcon className={styles.Icon} />
           <div className={styles.menudata}>Q&A 답변하기</div>
-        </div>
+        </Link>
         <div className={styles.container2}>
           <RuleIcon className={styles.Icon} />
           <div className={styles.menudata}>출결 관리</div>
